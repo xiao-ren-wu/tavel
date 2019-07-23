e66d540bb69c077845ba76d9c8ea455badc1ac38
xiao-ren-wu <18629015421@163.com>
2019-07-23 08:02:02 +0800

    ,

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,23 +1,45 @@
 <template>
-    <div class="banner">
-      <img class="banner-img" src="static/images/weekend/1.jpg"/>
-      <div class="banner-info">
-        <div class="banner-title">
-          大连海洋水世界（AAAA景区）
+    <div>
+      <div class="banner" @click="handleBannerClick">
+        <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"/>
+        <div class="banner-info">
+          <div class="banner-title">
+            大连海洋水世界（AAAA景区）
+          </div>
+          <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>39</div>
         </div>
-        <div class="banner-number"><span class="iconfont">&#xe632;</span>39</div>
       </div>
+<!--      <common-gallary :imgs="imgs"></common-gallary>-->
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Banner'
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'Banner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+
+      ]
     }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
+  // 防止页面抖动
   .banner
+    position : relative
     overflow : hidden
     height : 0
     padding-bottom : 55%
@@ -30,4 +52,19 @@
       right : 0
       bottom : 0
       line-height : .6rem
+      color : #fff
+      .banner-title
+        flex: 1
+        font-size : .32rem
+        padding : 0 .2rem
+      .banner-number
+        margin-top : .14rem
+        padding : 0 .4rem
+        line-height : .32rem
+        height: .4rem
+        border-radius : .2rem
+        background : rgba(0, 0, 0, .8)
+        font-size : .24rem
+        .banner-icon
+          font-size : .24rem
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
-import Details from '@pages/detail/Details'
+import Details from '@/pages/detail/Details'
 
 Vue.use(Router)
 
@@ -16,7 +16,8 @@ export default new Router({
     name: 'City',
     component: City
   }, {
-    path: '/detail',
+    // 动态路由，代表前面的路径必须是/detail,后面的路径可以是/id
+    path: '/detail/:id',
     name: 'Details',
     component: Details
   }]
