67cb0bfbc9b85c07706472aada401d786ffdf0b6
xiao-ren-wu <18629015421@163.com>
2019-07-23 08:40:31 +0800

    .

--- src/common/gallary/Gallary.vue
+++ src/common/gallary/Gallary.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="container">
-      <div class="wrapper">
-        <swiper :options="swiperOptions">
-          <swiper-slide
-            v-for="(item,index) in imgs"
-            :key="index"
-          >
-            <img class="gallary-img" src="item">
-          </swiper-slide>
-        </swiper>
-      </div>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <swiper-slide
+          v-for="(item, index) in imgs"
+          :key="index"
+        >
+          <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
     </div>
+  </div>
 </template>
 
 <script>
@@ -28,8 +29,15 @@ export default {
     return {
       swiperOptions: {
         pagination: '.swiper-pagination',
-        paginationType: 'fraction'
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
       }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
     }
   }
 }
@@ -37,26 +45,25 @@ export default {
 
 <style lang="stylus" scoped>
   .container >>> .swiper-container
-    overflow : inherit
+    overflow: inherit
   .container
-    display : flex
-    flex-direction : column
-    justify-content : center
-    z-index : 99
-    position : fixed
-    left : 0
-    right : 0
-    top : 0
-    bottom : 0
-    background : #000
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
     .wrapper
-      overflow : hidden
       height: 0
       width: 100%
-      padding-bottom : 100%
+      padding-bottom: 100%
       .gallary-img
         width: 100%
       .swiper-pagination
-        color : #ffffff
-        bottom : -1rem
+        color: #fff
+        bottom: -1rem
 </style>
